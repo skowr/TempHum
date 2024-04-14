ff1ff8095035e5237414162c15d013b990e07569
skowr <skowron.on.line@gmail.com>
2024-04-14 21:15:45 +0200

    Clean of code Added time handling

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 ##############################################
 #
-# TEMPHUM - SKR v0.3
+# TEMPHUM - SKR v0.32
 #
-# 12.04.2024
+# last update 14.04.2024
 #
 ##############################################
 
 import network
 import secrets
 import ubinascii
+import ntptime
+import time
 from machine import Pin
 from time import sleep
 from time import time
@@ -34,23 +36,13 @@ SAVE_TO_LOG = False
 # Reconnect WIFI ticks count
 WIFI_RECONNECT = 100
 
-
 # WIFI AND MQTT PARAMETERS"
-#MQTT_BROKER = "192.168.2.10"
-#MQQT_PORT = 1883
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
-#MQTT_TOPIC_TEMP_IN = b"drewniak/saloon/temperature"
-#MQTT_TOPIC_HUMID_IN = b"drewniak/saloon/humidity"
-#MQTT_TOPIC_TEMP_OUT = b"drewniak/garden/temperature"
-#MQTT_TOPIC_HUMID_OUT = b"drewniak/garden/humidity"
-
-
 
 # PINS DEFINITIONS
 sensor1 = dht.DHT11(Pin(0))
 sensor2 = dht.DHT11(Pin(4))
 intled = machine.Pin("LED", machine.Pin.OUT)
-
 
 wlan = network.WLAN(network.STA_IF)
 
@@ -97,87 +89,6 @@ def log(input):
         f.write(s+"\n")
         f.close()
 
-def test_sensors():
-    while True:
-      try:
-        sleep(2)
-
-        # SENSOR 1
-        sensor1.measure()
-        temp = sensor1.temperature()
-        hum = sensor1.humidity()
-        print('** Sensor 1: *** ')
-        print('Temperature: %3.1f C' %temp)
-        print('Humidity: %3.1f %%' %hum)
-        
-        # SENSOR 2
-        sensor2.measure()
-        temp = sensor2.temperature()
-        hum = sensor2.humidity()
-        print('** Sensor 2: *** ')
-        print('Temperature: %3.1f C' %temp)
-        print('Humidity: %3.1f %%' %hum)
-
-        
-      except OSError as e:
-        print('Failed to read sensor.')
-
-def test_connect_wifi():
-    
-    wlan.active(True)
-    wlan.connect(secrets.WIFI_SSID, secrets.WIFI_PASS)
-    while not wlan.isconnected():
-        sleep(1)
-        
-    print("Connected: ", wlan.isconnected())
-    print("Connection details: ", wlan.ifconfig())
-    
-    if wlan.status() == 3: 
-        intled.value(1)
-    else:
-        intled.value(0)
-    
-    
-def test_connect_mqtt():
-
-    print("Connecting MQTT. Client ID: ", CLIENT_ID)
-
-    mqttClient = MQTTClient(CLIENT_ID, secrets.MQTT_BROKER, port=SECR, user=secrets.MQTT_USER, password=secrets.MQTT_PASS, keepalive=60)
-    mqttClient.connect()
-
-    while True:
-        try:
-            sleep(5)
-            
-            # SENSOR 1
-            sensor1.measure()
-            temp = sensor1.temperature()
-            hum = sensor1.humidity()
-            print('Temperature: %3.1f C' %temp)
-            print('Humidity: %3.1f %%' %hum)
-            
-
-
-            # SENSOR 2
-            sensor2.measure()
-            temp = sensor2.temperature()
-            hum = sensor2.humidity()
-            print('Temperature: %3.1f C' %temp)
-            print('Humidity: %3.1f %%' %hum)
-
-
-            mqttClient.publish(MQTT_TOPIC_TEMP_IN, str(temp).encode())
-            print("MQTT published temperature")
-
-            mqttClient.publish(MQTT_TOPIC_HUMID_IN, str(hum).encode())
-            print("MQTT published humidity")
-
-
-        except OSError as e:
-            print('Failed to read sensor.')
-
-   
-
 def publish_to_mqtt():
     try:
        
@@ -189,9 +100,9 @@ def publish_to_mqtt():
             hum = sensor1.humidity()        
             log("Drewniak Temperature: %3.1f C" %temp)
             log("Drewniak Humidity: %3.1f %%" %hum)
-            mqttClient.publish(MQTT_TOPIC_TEMP_IN, str(temp).encode())
+            mqttClient.publish(secrets.MQTT_TOPIC_TEMP_IN, str(temp).encode())
             log("[OK] MQTT published temperature")
-            mqttClient.publish(MQTT_TOPIC_HUMID_IN, str(hum).encode())
+            mqttClient.publish(secrets.MQTT_TOPIC_HUMID_IN, str(hum).encode())
             log("[OK] MQTT published humidity")
         
 
@@ -204,9 +115,9 @@ def publish_to_mqtt():
             log("Outside Temperature: %3.1f C" %temp)
             log("Outside Humidity: %3.1f %%" %hum)
             
-            mqttClient.publish(MQTT_TOPIC_TEMP_OUT, str(temp).encode())
+            mqttClient.publish(secrets.MQTT_TOPIC_TEMP_OUT, str(temp).encode())
             log("[OK] MQTT published temperature")
-            mqttClient.publish(MQTT_TOPIC_HUMID_OUT, str(hum).encode())
+            mqttClient.publish(secrets.MQTT_TOPIC_HUMID_OUT, str(hum).encode())
             log("[OK] MQTT published humidity")
             
         # Blink to confirm
@@ -222,6 +133,16 @@ def connect_wifi():
     wlan.active(True)
     wlan.connect(secrets.WIFI_SSID, secrets.WIFI_PASS)
 
+
+def set_global_time():
+    log("Connecting NTP: " + secrets.NTP_SERVER)
+    ntptime.host = secrets.NTP_SERVER
+    try:
+        ntptime.settime()
+        log("Local time after synchronization：" + str(time.localtime()))
+    except:
+        log("[ER] Cannot connect time server")
+    
 
 def status_light():
     global app_counter
@@ -256,7 +177,7 @@ def controller():
     # INIT
     if app_counter == 0:
         log("***  TEMPHUM   ***")
-        log("version 12.04.2024")
+        log("v 0.32")
         log(" ")
         app_stat = App_status.INITIATION
     elif app_counter < 10:
@@ -277,16 +198,17 @@ def controller():
             elif app_stat == App_status.WAIT_WIFI:
                 # ... AND CONNECT MQTT
                 if wlan.isconnected():
-                    log("[OK] WiFi connected: " + str(wlan.ifconfig()))
+                    log("[OK] WiFi connected: " + str(wlan.ifconfig()))                    
+                    #set_global_time()                    
                     app_stat = App_status.CONN_MQTT
                 elif led_ctrl.counter > WIFI_RECONNECT:
                     app_stat = App_status.CONN_WIFI
 
             # WIFI CONNECTED
             elif app_stat == App_status.CONN_MQTT:
-                log("Connecting MQTT: Client: " + str(CLIENT_ID) + " , Broker: " + MQTT_BROKER + " , Port: " + str(MQQT_PORT))
+                log("Connecting MQTT: Client: " + str(CLIENT_ID) + " , Broker: " + secrets.MQTT_BROKER + " , Port: " + str(secrets.MQQT_PORT))
 
-                mqttClient = MQTTClient(CLIENT_ID, MQTT_BROKER, port=1883, user=secrets.MQTT_USER, password=secrets.MQTT_PASS, keepalive=SAMPL_PERIOD+10)    
+                mqttClient = MQTTClient(CLIENT_ID, secrets.MQTT_BROKER, secrets.MQQT_PORT, user=secrets.MQTT_USER, password=secrets.MQTT_PASS, keepalive=SAMPL_PERIOD+10)    
                 mqttClient.connect()
                 app_stat = App_status.WAIT_MQTT
             
